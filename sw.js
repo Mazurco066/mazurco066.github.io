2b16b020059173368b26bd3d8e3a5bacad97c5ab
Gabriel Ribeiro <mazurco066@gmail.com>
2021-01-10 10:11:18 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b4b2ad67f35ab45b9b27.js"
+    "url": "webpack-runtime-41a291816749d8d3a873.js"
   },
   {
     "url": "framework-c820244bc434f531519f.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ac49ae3adccfedd6bd031f1a21683a7c"
+    "revision": "2923b6ceb1b6ef57cbdcd527ff5172bc"
   },
   {
     "url": "google-fonts/s/montserrat/v14/JTURjIg1_i6t8kCHKm45_bZF3gnD_g.woff2",
@@ -72,7 +72,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "ca3eb47a81b7f939ffa334def360248d"
+    "revision": "c8a7645075505cc751a1fa281f07a3c9"
   },
   {
     "url": "manifest.webmanifest",

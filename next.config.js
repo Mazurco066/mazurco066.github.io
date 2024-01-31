da275a84975a6df16d51a112c0f9002597bed192
Gabriel Mazurco (RaktaTech) <mazurco066@gmail.com>
2024-01-31 14:22:45 -0300

    fix(next): config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: process.env.NODE_ENV === 'production' ? "" : undefined,
   images: {
     unoptimized: true,

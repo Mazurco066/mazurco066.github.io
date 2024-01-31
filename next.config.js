4e3ef08814ff0d0eafc064f36d3dfcb9389117cd
Gabriel Mazurco (RaktaTech) <mazurco066@gmail.com>
2024-01-31 13:05:49 -0300

    fix(ghpages): fixing next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
   images: {
     remotePatterns: [
       {

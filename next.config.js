bd32a8b384abedd596d27b8cfb8a2220e3c0cb62
Gabriel Mazurco (RaktaTech) <mazurco066@gmail.com>
2024-01-31 14:02:43 -0300

    fix(nextjs): build image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === 'production' ? "" : undefined,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
